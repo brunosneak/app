62bd32815bc8c8f646d836f725501d189e0cca59
bruno <brunosneak@gmail.com>
2019-11-29 17:02:08 +0100

    Maj tour du jeu

--- js/jeu.js
+++ js/jeu.js
@@ -2,30 +2,44 @@
 
 window.addEventListener("load", function(){
 
-    var monHero = document.getElementById("hero");
-    var positionLeftHero = monHero.style.left;
-    // var positionYHero = monHero.offsetTop;
+    // var monHero = document.getElementById("hero");
+    // var positionLeftHero = monHero.offsetLeft;
+    // // var positionYHero = monHero.offsetTop;
 
 
-    window.addEventListener("keydown", function(event){
+    // window.addEventListener("keydown", function(event){
 
-        var position = event.keyCode;
+    //     var position = event.keyCode;
 
-        switch(position){
-            // flèche directionnelle gauche
-            case 37:
-                positionLeftHero -= 50;
-                monHero.style.left = positionLeftHero + "px";
-                monHero.style.transform = "";
-            break;
-            // flèche directionnelle droite
-            case 39:
-                positionLeftHero += 50;
-                monHero.style.left = positionLeftHero + "px";
-                monHero.style.transform = "scaleX(-1)";
-            break;
-        }
+    //     switch(position){
+    //         // flèche directionnelle gauche
+    //         case 37:
+    //             positionLeftHero -= 20;
+    //             monHero.style.left = positionLeftHero + "px";
+    //             monHero.style.transform = "";
+    //         break;
+    //         // flèche directionnelle droite
+    //         case 39:
+    //             positionLeftHero += 20;
+    //             monHero.style.left = positionLeftHero + "px";
+    //             monHero.style.transform = "scaleX(-1)";
+    //         break;
+    //     }
 
-    });
+    // });
+
+    window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    
+    var fenetre = document.getElementById('fenetreGauche');
+
+    var fenetreAnimee = function(){
+        
+        var positionFenetre = fenetre.offsetTop;
+            positionFenetre -= 2;
+            fenetre.style.top = positionFenetre + "px";
+
+            requestAnimationFrame(fenetreAnimee);
+    };
 
 });
