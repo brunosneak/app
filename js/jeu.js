002c1622bf1002efce144770cc8b1b501af661d0
bruno <brunosneak@gmail.com>
2019-12-04 16:28:07 +0100

    MAJ refacto

--- js/jeu.js
+++ js/jeu.js
@@ -1,37 +1,37 @@
 "use strict";
 
-window.addEventListener("load", function(){
+/*********************************************************************************************************************/
+/*********************************************INITIALISATION**********************************************************/
+/*********************************************************************************************************************/
 
-/*********************************************************************************************************************/
-/*****************************************REQUEST ANIMATION FRAME*****************************************************/
-/*********************************************************************************************************************/
+var decors = [document.getElementById('fenetreGauche'),document.getElementById('fenetreDroite'),document.getElementById('pieceJs'),document.getElementById('boulePique'),document.getElementById('masqueHero'),document.getElementById('font_vert'),document.getElementById('hero'),document.getElementById('masqueFantome'),document.getElementById('fantome'),document.getElementById('masqueInsecte'),document.getElementById('insecte'),document.getElementById('masqueInsecte2'),document.getElementById('insecte2')];
+var masque = decors[4];
+var hero = decors[6];
+var heroLeft = hero.style.left;
+var fantome = decors[8];
+var fantomeLeft = fantome.style.left;
+var insecte = decors[10];
+var insecteLeft = insecte.style.left;
+var insecte2 = decors[12];
+var insecte2Left = insecte2.style.left;
     
-    window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
 /*************************************************************************************************************************/
 /***************************************POSITIONNEMENT DEPART DU DECORS***************************************************/
 /*************************************************************************************************************************/
 
     var animationDuJeu = function(){
-    var decors = [document.getElementById('fenetreGauche'),document.getElementById('fenetreDroite'),document.getElementById('pieceJs'),document.getElementById('boulePique'),document.getElementById('masqueHero'),document.getElementById('font_vert'),document.getElementById('hero'),document.getElementById('masqueFantome'),document.getElementById('fantome'),document.getElementById('masqueInsecte'),document.getElementById('insecte'),document.getElementById('masqueInsecte2'),document.getElementById('insecte2')];
-    
-    var positionDeDepartFenetre = function(element1, element2){
-        element1.style.bottom = "-" + Math.floor(Math.random()*1000) - element1.offsetHeight + "px";
-        element2.style.bottom = element1.style.bottom;
-    }
-    
-    var positionDeDepartObjet = function(element){
-        element.style.bottom = "-" + Math.floor(Math.random()*1000) - element.offsetHeight + "px";
-    }
-
-    positionDeDepartFenetre(decors[0], decors[1]);
-    positionDeDepartObjet(decors[2]);
-    positionDeDepartObjet(decors[3]);
-    positionDeDepartObjet(decors[7]);
-    positionDeDepartObjet(decors[9]);
-    positionDeDepartObjet(decors[11]);
-
+        
+        var positionDeDepartFenetre = function(element1, element2){
+            element1.style.bottom = "-" + Math.floor(Math.random()*1000) - element1.offsetHeight + "px";
+            element2.style.bottom = element1.style.bottom;
+        }
+        
+        var positionDeDepartObjet = function(element){
+            element.style.bottom = "-" + Math.floor(Math.random()*1000) - element.offsetHeight + "px";
+        }
 
 /*************************************************************************************************************************/
 /*********************************************ANIMATION DU DECORS*********************************************************/
@@ -100,17 +100,12 @@ window.addEventListener("load", function(){
             decors[11].style.top = fond_vert.offsetHeight + Math.floor(Math.random()*1000) + "px";
         }
     };
-    slideUp();
-
 
 /*************************************************************************************************************************/
 /******************************************DEPLACEMENT DU PERSONNAGE******************************************************/
 /*************************************************************************************************************************/
 
-    var masque = decors[4];
     var coordonneesMasque = masque.getBoundingClientRect();
-    var positionX = coordonneesMasque.x;
-    var positionY = coordonneesMasque.y;
 
     window.addEventListener("keydown", function(event){
 
@@ -119,25 +114,25 @@ window.addEventListener("load", function(){
         switch(position){
             // flèche directionnelle gauche
             case 37:
-                positionX -= 10;
-                masque.style.left = positionX + "px";
+                coordonneesMasque.x -= 10;
+                masque.style.left = coordonneesMasque.x + "px";
                 masque.style.transform = "";
             break;
             // flèche directionnelle haut
             case 38:
-                positionY -= 10;
-                masque.style.top = positionY + "px";
+                coordonneesMasque.y -= 10;
+                masque.style.top = coordonneesMasque.y + "px";
             break;
             // flèche directionnelle droite
             case 39:
-                positionX += 10;
-                masque.style.left = positionX + "px";
+                coordonneesMasque.x += 10;
+                masque.style.left = coordonneesMasque.x + "px";
                 masque.style.transform = "scaleX(-1)";
             break;
             // flèche directionnelle bas
             case 40:
-                positionY += 10;
-                masque.style.top = positionY + "px";
+                coordonneesMasque.y += 10;
+                masque.style.top = coordonneesMasque.y + "px";
             break
         };
 
@@ -147,77 +142,51 @@ window.addEventListener("load", function(){
 /*******************************************ANIMATION DU PERSONNAGE*******************************************************/
 /*************************************************************************************************************************/
 
-    var hero = decors[6];
-    var heroLeft = hero.style.left;
     var mouvementHero = function(){
-        setTimeout(function(){
-            heroLeft -= 120;
-            if(heroLeft < -360){
-                heroLeft = -14;
-            }
-            hero.style.left = heroLeft + "px";
-            requestAnimationFrame(mouvementHero);
-        }, 1000 / 10);
+        heroLeft -= 120;
+        if(heroLeft < -360){
+            heroLeft = -14;
+        };
+        hero.style.left = heroLeft + "px";
     };
-    mouvementHero();
-
+    
 /*************************************************************************************************************************/
 /********************************************ANIMATION DU FANTOME*********************************************************/
 /*************************************************************************************************************************/
 
-    var fantome = decors[8];
-    var fantomeLeft = fantome.style.left;
-
     var mouvementFantome = function(){
-        setTimeout(function(){
-            fantomeLeft -= 120;
-            if(fantomeLeft < -840){
-                fantomeLeft = 0;
-            }
-            fantome.style.left = fantomeLeft + "px";
-            requestAnimationFrame(mouvementFantome);
-        }, 1000 / 10);
+        fantomeLeft -= 120;
+        if(fantomeLeft < -840){
+            fantomeLeft = 0;
+        };
+        fantome.style.left = fantomeLeft + "px";
     };
-    mouvementFantome();
-
+    
 /*************************************************************************************************************************/
 /*******************************************ANIMATION DE L'INSECTE********************************************************/
 /*************************************************************************************************************************/
 
-    var insecte = decors[10];
-    var insecteLeft = insecte.style.left;
-
     var mouvementInsecte = function(){
-        setTimeout(function(){
-            insecteLeft -= 286;
-            if(insecteLeft < -4290){
-                insecteLeft = 0;
-            }
-            insecte.style.left = insecteLeft + "px";
-            requestAnimationFrame(mouvementInsecte);
-        }, 1000 / 10);
+        insecteLeft -= 286;
+        if(insecteLeft < -4290){
+            insecteLeft = 0;
+        };
+        insecte.style.left = insecteLeft + "px";
     };
-    mouvementInsecte();
+    
 
 /*************************************************************************************************************************/
 /******************************************ANIMATION DE L'INSECTE 2*******************************************************/
 /*************************************************************************************************************************/
 
-    var insecte2 = decors[12];
-    var insecte2Left = insecte2.style.left;
-
     var mouvementInsecte2 = function(){
-        setTimeout(function(){
-            insecte2Left -= 286;
-            if(insecte2Left < -4290){
-                insecte2Left = 0;
-            }
-            insecte2.style.left = insecte2Left + "px";
-            requestAnimationFrame(mouvementInsecte2);
-        }, 1000 / 10);
+        insecte2Left -= 286;
+        if(insecte2Left < -4290){
+            insecte2Left = 0;
+        };
+        insecte2.style.left = insecte2Left + "px";
     };
-    mouvementInsecte2();
-
+    
 
 /*************************************************************************************************************************/
 /********************************************DETECTEUR DE COLLISION*******************************************************/
@@ -225,10 +194,6 @@ window.addEventListener("load", function(){
 
     var coordonneesFantome = decors[7].getBoundingClientRect();
     var coordonneesBoulePique = decors[3].getBoundingClientRect();
-    
-    // if(coordonneesMasque.y + coordonneesMasque.height && coordonneesMasque.y + coordonneesMasque.height + coordonneesMasque.x + coordonneesMasque.width == coordonneesFantome.x + coordonneesFantome.y && coordonneesFantome.x + coordonneesFantome.width){
-    //     alert("tu es mort");
-    // };
 
     if(coordonneesMasque.x < coordonneesFantome.x + coordonneesFantome.width &&
         coordonneesMasque.x + coordonneesMasque.width > coordonneesFantome.x &&
@@ -242,10 +207,28 @@ window.addEventListener("load", function(){
         alert("tu es mort");
     };
 
+/*************************************************************************************************************************/
+/*******************************************EXECUTION DES FONCTIONS*******************************************************/
+/*************************************************************************************************************************/
+
+    positionDeDepartFenetre(decors[0], decors[1]);
+    positionDeDepartObjet(decors[2]);
+    positionDeDepartObjet(decors[3]);
+    positionDeDepartObjet(decors[7]);
+    positionDeDepartObjet(decors[9]);
+    positionDeDepartObjet(decors[11]);
+    slideUp();
+    mouvementHero();
+    mouvementFantome();
+    mouvementInsecte();
+    mouvementInsecte2();
     requestAnimationFrame(animationDuJeu);
+};
 
-    };
+/*************************************************************************************************************************/
+/*****************************************AU CHARGEMENT DE LA FENETRE*****************************************************/
+/*************************************************************************************************************************/
 
+window.addEventListener("load", function(){
     animationDuJeu();
-
 });
--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ window.addEventListener("load", function(){
         firstLine[0].style = "left: 5px; transform: rotate(45deg); width: 53%; transform-origin: top left; transition: transform .5s, width .5s, left .5s";
         secondLine[0].style = "opacity: 0; transition: opacity .5s";
         thirdLine[0].style = "left: 4px; transform: rotate(-45deg); width: 53%; transform-origin: top left; transition: transform .5s, width .5s, left .5s";
-
     };
 
     function animationBoutonBurgerRetour(){
@@ -26,7 +25,6 @@ window.addEventListener("load", function(){
         firstLine[0].style = "left: 0; transform: rotate(0); width: 50%; transform-origin: top left; transition: transform .5s, width .5s, left .5s";
         secondLine[0].style = "opacity: 1; transition: opacity .5s";
         thirdLine[0].style = "left: 0; transform: rotate(0); width: 50%; transform-origin: top left; transition: transform .5s, width .5s, left .5s";
-
     };
 
     function auCliqueDuBouton(){
@@ -60,4 +58,5 @@ window.addEventListener("load", function(){
     //     });
     // };
     // mainParallax();
+    
 });
