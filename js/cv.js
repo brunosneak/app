b636523854b585b5ed058cbdf528a084f9d6ff00
bruno <brunosneak@gmail.com>
2019-12-18 21:32:18 +0100

    refacto page cv

--- js/cv.js
+++ js/cv.js
@@ -1,3 +1,7 @@
+/*************************************************************************************************************************/
+/******************************************ANIMATION PROGRESS BAR CIRCLE**************************************************/
+/*************************************************************************************************************************/
+
 var monCercle = function(arg1, monImg, taille, color){
 
     var monCanvas = document.createElement('canvas');
@@ -29,28 +33,28 @@ var monCercle = function(arg1, monImg, taille, color){
 
 // progressBarCircle css
 monCercle('css',"", 100,'#4c4c4c');
-monCercle('css', "url('img/img_cv/css.svg')", 65, "#fff");
+monCercle('css', "url('img/imgCv/css.svg')", 65, "#fff");
 // progressBarCircle html
 monCercle('html5','', 100,'#4c4c4c');
-monCercle('html5', "url('img/img_cv/html5.svg')", 80, "#fff");
+monCercle('html5', "url('img/imgCv/html5.svg')", 80, "#fff");
 // progressBarCircle sass
 monCercle('sass', "", 100, '#4c4c4c');
-monCercle('sass', "url('img/img_cv/sass.svg')", 60, "#fff");
+monCercle('sass', "url('img/imgCv/sass.svg')", 60, "#fff");
 // progressBarCircle js
 monCercle('js', "", 100, '#4c4c4c');
-monCercle('js', "url('img/img_cv/js.svg')", 70, "#fff");
+monCercle('js', "url('img/imgCv/js.svg')", 70, "#fff");
 // progressBarCircle jquery
 monCercle('jquery', "", 100, '#4c4c4c');
-monCercle('jquery', "url('img/img_cv/jquery.svg')", 40, "#fff");
+monCercle('jquery', "url('img/imgCv/jquery.svg')", 40, "#fff");
 // progressBarCircle angular
 monCercle('angular', "", 100, '#4c4c4c');
-monCercle('angular', "url('img/img_cv/angular.svg')", 30, "#fff");
+monCercle('angular', "url('img/imgCv/angular.svg')", 30, "#fff");
 // progressBarCircle node
 monCercle('node', "", 100, '#4c4c4c');
-monCercle('node', "url('img/img_cv/node.svg')", 20, "#fff");
+monCercle('node', "url('img/imgCv/node.svg')", 20, "#fff");
 // progressBarCircle git
 monCercle('github', "", 100, '#4c4c4c');
-monCercle('github', "url('img/img_cv/github.svg')", 55, "#fff");
+monCercle('github', "url('img/imgCv/github.svg')", 55, "#fff");
 // progressBarCircle mongodb
 monCercle('mongodb', "", 100, '#4c4c4c');
-monCercle('mongodb', "url('img/img_cv/mongodb.svg')", 20, "#fff");
+monCercle('mongodb', "url('img/imgCv/mongodb.svg')", 20, "#fff");
